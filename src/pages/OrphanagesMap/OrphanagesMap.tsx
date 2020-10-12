1d2a74af4ae05cf81f94d410bd9eef69463cb285
Drazyn <gabriellucena360@outlook.com>
2020-10-12 11:56:19 -0300

    Conceitos e Estrutura Web / Aula 1

---
+++ src/pages/OrphanagesMap/OrphanagesMap.tsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import {Link} from 'react-router-dom';
+import {FiPlus} from 'react-icons/fi';
+
+import { Map, TileLayer } from 'react-leaflet';
+import 'leaflet/dist/leaflet.css';
+
+import mapMarkerImg from '../../images/map-marker.svg';
+
+import '../../styles/pages/orphanagemap.css';
+
+function OrphanagesMap() {
+    return (
+        <div id="page-map">
+            <aside>
+                <header>
+                    <img src={mapMarkerImg} alt="Map Marker"/>
+                    <h2>Escolha um orfanato no mapa</h2>
+                    <p>Muitas crianças estão esperando a sua visita :)</p>
+                </header>
+                <footer>
+                    <strong>Fortaleza</strong>
+                    <span>Ceará</span>
+                </footer>
+            </aside>
+
+            <Map 
+                center={[-3.728917,-38.4744778]}
+                zoom={15}
+                style={{width: '100%', height: '100%'}}
+            >
+                <TileLayer url="https://a.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+            </Map>
+
+            <Link to="" className="create-orphanage">
+                <FiPlus size={32} color="#FFF" />
+            </Link>
+
+        </div>
+    );
+}
+
+export default OrphanagesMap;
